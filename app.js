1a1a474c0a4785fc324c46b26edb3b45b5239677
alicjapol <alicjaxpoltorak@gmail.com>
2024-05-06 20:55:18 +0200

    verify

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+require('dotenv').config();
+console.log(process.env.MAILGUN_API_KEY)
 
-
-const userRoutes = require('./routes/users');
-const contactsRouter = require('./routes/contacts');
+const userRoutes = require("./routes/users");
+const contactsRouter = require("./routes/contacts");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri =
-  'mongodb+srv://alicjaxpoltorak:master3@cluster0.zaf91zr.mongodb.net/';
+  "mongodb+srv://alicjaxpoltorak:master3@cluster0.zaf91zr.mongodb.net/";
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Database connection successful'))
+  .then(() => console.log("Database connection successful"))
   .catch((err) => {
-    console.error('Database connection error', err);
+    console.error("Database connection error", err);
     process.exit(1);
   });
 
-app.use('/users', userRoutes);
-app.use('/contacts', contactsRouter);
+app.use("/users", userRoutes);
+app.use("/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
+
+
 
 module.exports = app;
--- services/email.service.js
+++ services/email.service.js
@@ -1,6 +1,8 @@
 const mailgun = require('mailgun-js');
-const DOMAIN = 'sandboxd3a08a7b3ce34373b3ec6610b2e933c5.mailgun.org';
+const DOMAIN = 'sandboxd3a08a7b3ce34373b3ec6610b2e933c5.mailgun.org'
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
+
+
 
 const sendVerificationEmail = (email, verificationToken) => {
   const data = {
